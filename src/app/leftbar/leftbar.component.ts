6123d0fd82bd315797034c3f26c11e9701f0f24b
woraprat <woraprat.bo.56@ubu.ac.th>
2017-09-07 17:02:03 +0700

    update

--- src/app/leftbar/leftbar.component.ts
+++ src/app/leftbar/leftbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-leftbar',
   templateUrl: './leftbar.component.html',
-  styleUrls: ['./leftbar.component.css']
+  styleUrls: ['./leftbar.component.scss']
 })
 export class LeftbarComponent implements OnInit {
 
--- src/app/rightbar/rightbar.component.ts
+++ src/app/rightbar/rightbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rightbar',
   templateUrl: './rightbar.component.html',
-  styleUrls: ['./rightbar.component.css']
+  styleUrls: ['./rightbar.component.scss']
 })
 export class RightbarComponent implements OnInit {
 
