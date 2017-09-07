642c13896c9a99d316a7c3eafaa6c7da33017902
woraprat <woraprat.bo.56@ubu.ac.th>
2017-09-07 17:01:33 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LeftbarComponent } from './leftbar/leftbar.component';
+import { RightbarComponent } from './rightbar/rightbar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LeftbarComponent,
+    RightbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
